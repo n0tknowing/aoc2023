7ff30601371171d26acdf4e355411c3505b2b401
Widianto Nur F <xnaltasee@gmail.com>
2023-12-07 11:40:50 +0700

    Cleanup day 6

--- 06.py
+++ 06.py
@@ -8,22 +8,16 @@ def solve(time, distance):
     return win
 
 lines = open("06.txt", "r").read().strip().split("\n")
-times = [int(i) for i in lines[0][lines[0].index(":") + 2:].split()]
-distances = [int(i) for i in lines[1][lines[1].index(":") + 2:].split()]
-p1_a = []
-
-for i in range(len(times)):
-    time = times[i]
-    distance = distances[i]
-    p1_a.append(solve(time, distance))
-
-p1 = p1_a[0]
-for p in range(1, len(p1_a)):
-    p1 *= p1_a[p]
-
-print(p1)
-
+p1_time = [int(i) for i in lines[0][lines[0].index(":") + 2:].split()]
+p1_distance = [int(i) for i in lines[1][lines[1].index(":") + 2:].split()]
 p2_time = int(lines[0][lines[0].index(":") + 2:].replace(" ", ""))
 p2_distance = int(lines[1][lines[1].index(":") + 2:].replace(" ", ""))
+
+p1 = 1
+for i in range(len(p1_time)):
+    time = p1_time[i]
+    distance = p1_distance[i]
+    p1 *= solve(time, distance)
+
 p2 = solve(p2_time, p2_distance)
-print(p2)
+print(p1, p2)
