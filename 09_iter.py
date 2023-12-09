cb1c7c43c0a69d4c0cba7143046d9692813516ed
Widianto Nur F <xnaltasee@gmail.com>
2023-12-09 13:37:09 +0700

    You don't need list `c`

--- 09_iter.py
+++ 09_iter.py
@@ -1,16 +1,16 @@
 lines = open("09.txt").read().strip().split("\n")
 
 def solve(a):
-    c = []
+    res = 0
     while True:
         if all(i == 0 for i in a):
             break
         b = []
         for i in range(len(a)-1):
             b.append(a[i+1]-a[i])
-        c.append(a[-1])
+        res += a[-1]
         a = b
-    return sum(c)
+    return res
 
 p1 = 0
 p2 = 0
